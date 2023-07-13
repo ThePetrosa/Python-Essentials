002fb8a10db4595e70df7d3d772414fabe01c3a7
Alejandra Pilamunga <137963172+ThePetrosa@users.noreply.github.com>
2023-07-12 21:19:40 -0500

    Create strg.py

---
+++ strg.py
@@ -0,0 +1,6 @@
+name=input("Ingrese nombre: ")
+lastname=input("Ingrese apellido: ")
+location=input("Ingrese la direccion: ")
+age=input("Ingrese la edad: ")
+space=" "
+print(name+space+lastname+space+location+space+age)
